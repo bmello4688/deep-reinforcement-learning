9b6d0bf6890f5d1eaf7cbfb5daab3ed2df49edb5
bmello4688 <brian.mello@outlook.com>
2020-12-16 09:39:23 -0500

    fix model can't get TD3 to work

--- ddpg-pendulum/model.py
+++ ddpg-pendulum/model.py
@@ -75,46 +75,32 @@ class Critic(nn.Module):
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
         
-        #Q1
-        self.fcs1q1 = nn.Linear(state_size+action_size, fcs1_units)
-        self.fc2q1 = nn.Linear(fcs1_units, fc2_units)
-        self.fc3q1 = nn.Linear(fc2_units, 1)
+        self.fcs1 = nn.Linear(state_size+action_size, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
         
         self.bn1q1 = nn.BatchNorm1d(fcs1_units)
         self.bn2q1 = nn.BatchNorm1d(fc2_units)     
-        #self.bn3q1 = nn.BatchNorm1d(1)  
-        
-        #Q2
-        #self.fcs1q2 = nn.Linear(state_size+action_size, fcs1_units)
-        #self.fc2q2 = nn.Linear(fcs1_units, fc2_units)
-        #self.fc3q2 = nn.Linear(fc2_units, 1)
-        
-        #self.bn1q2 = nn.BatchNorm1d(fcs1_units)
-        #self.bn2q2 = nn.BatchNorm1d(fc2_units)     
-        #self.bn3q2 = nn.BatchNorm1d(1)  
         
         self.reset_parameters()
 
     def reset_parameters(self):
-        self.fcs1q1.weight.data.uniform_(*hidden_init(self.fcs1q1))
-        self.fc2q1.weight.data.uniform_(*hidden_init(self.fc2q1))
-        self.fc3q1.weight.data.uniform_(-3e-3, 3e-3)
-        
-        #self.fcs1q2.weight.data.uniform_(*hidden_init(self.fcs1q2))
-        #self.fc2q2.weight.data.uniform_(*hidden_init(self.fc2q2))
-        #self.fc3q2.weight.data.uniform_(-3e-3, 3e-3)
+        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
-        """Build a duel critic (value) network that maps (state, action) pairs -> Q-values."""
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
         
-        state_action = torch.cat((state, action), dim=1)
-        
-        q1 = F.relu(self.bn1q1(self.fcs1q1(state_action)))
-        q1 = F.relu(self.bn2q1(self.fc2q1(q1)))
-        q1 = self.fc3q1(q1)
-        
-        #q2 = F.relu(self.bn1q2(self.fcs1q2(state_action)))
-        #q2 = F.relu(self.bn2q2(self.fc2q2(q2)))
-        #q2 = self.fc3q2(q2)
-        
-        return q1#, q2
+        if state.ndim == 1:
+            state = state.view(1, state.size(0))
+                  
+        x = self.fcs1(state)
+        x = self.bn1(x)   
+        x = F.relu(x)
+        x = torch.cat((x, action), dim=1)
+        x = self.fc2(x)
+        x = self.bn2(x) 
+        x = F.relu(x)
+
+        return self.fc3(x)
