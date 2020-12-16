24fd62bb69664d89ce35520a3694153be8cf2b8b
bmello4688 <brian.mello@outlook.com>
2020-12-16 09:26:38 -0500

    cleanup soft update

--- p2_continuous-control/ddpg_agent.py
+++ p2_continuous-control/ddpg_agent.py
@@ -147,7 +147,7 @@ class Agent():
         """
         
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-            target_param.data.copy_(tau*local_param.data + ((1.0-tau)*target_param.data))
+            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
 
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
